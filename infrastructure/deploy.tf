d85d309ab7d113fba1594aaed4cca1e076e0ed7b
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-16 09:11:30 +0000

    Update deploy.tf

--- infrastructure/deploy.tf
+++ infrastructure/deploy.tf
@@ -1,12 +1,12 @@
 variable "token" {}
-variable "image_id" {}
+variable "node_image_id" {}
 
 provider "digitalocean" {
     token = "${var.token}"
 }
 
 # Create a new droplet
-resource "digitalocean_droplet" "terraform" {
+resource "digitalocean_droplet" "node" {
     image  = "${var.image_id}"
     name   = "io-delineate-node-${var.image_id}"
     region = "lon1"
