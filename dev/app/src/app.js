f21e007349936f5bc45bc0461ff94d7b4e2403bb
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-16 16:10:39 +0000

    App & Reverse Proxy Changes

--- dev/app/src/app.js
+++ dev/app/src/app.js
@@ -7,8 +7,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
-var routes = require("./routes/routes.js")(app);
+var routes = require("./routes.js")(app);
  
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
     console.log("Listening on port %s...", server.address().port);
 });
