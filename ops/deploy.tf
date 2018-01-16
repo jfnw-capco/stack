6547b3ff812ea2299857a73d486753cf4ba53956
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-16 13:00:41 +0000

    Amendments to using branch to ensure ripples through

--- ops/deploy.tf
+++ ops/deploy.tf
@@ -9,7 +9,7 @@ provider "digitalocean" {
 # Create a new droplet
 resource "digitalocean_droplet" "node" {
     image  = "${var.node_image_id}"
-    name   = "io-delineate-node-${var.short_branch}-${count.index}"
+    name   = "io-delineate-node-${var.short_branch}-${count.index + 1}"
     region = "lon1"
     size   = "512mb"
 }
