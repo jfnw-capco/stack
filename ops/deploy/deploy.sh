187d36a1e4dbff2c522fc285e5583da383092b4d
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 10:08:11 +0000

    changes to parameterise

--- ops/deploy/deploy.sh
+++ ops/deploy/deploy.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
-# Retrieves the Image ID
-    if [ ! -d state ]; then
-        mkdir state
-    fi
-
-    cp ../provision/state/current_image_id.txt state/image_id.txt
-    IMAGE_ID=$(cat state/image_id.txt) 
     
 # initiatlises the directory
     terraform init
 
 # Validates the plan    
-    terraform validate -var "token=${DO_TOKEN}" -var "image_id=${IMAGE_ID}" 
+    terraform validate -var "token=${DO_TOKEN}" -var "image_id=${#1}" 
 
 # Outputs the the plan
-    terraform plan -var "token=${DO_TOKEN}" -var "image_id=${IMAGE_ID}" -out=terraform.plan -lock=true
+    terraform plan -var "token=${DO_TOKEN}" -var "image_id=${#1}" -out=terraform.plan -lock=true
 
 # Performs the changes to the infrastructure
-    terraform apply -var "token=${DO_TOKEN}" -var "image_id=${IMAGE_ID}" -auto-approve -lock=true
+    terraform apply -var "token=${DO_TOKEN}" -var "image_id=${#1}" -auto-approve -lock=true
