9efc734cfe3880603cd9a6d6063470eaf55df42b
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 10:51:52 +0000

    Ensures destroy is initialised and planned

--- ops/deploy/destroy.sh
+++ ops/deploy/destroy.sh
@@ -3,5 +3,14 @@
 # Ensures runs in the current directory 
     cd $(dirname "$0")
 
+# initiatlises the directory
+    terraform init
+
+# Validates the plan    
+    terraform validate -var "token=${DO_TOKEN}" -var "image_id=$1" 
+
+# Outputs the the plan
+    terraform plan -var "token=${DO_TOKEN}" -var "image_id=$1" -destroy -out=terraform.plan -lock=true
+
 # Performs the destroy 
     terraform destroy -var "token=${DO_TOKEN}" -var "image_id=$1" -refresh=true -force -lock=true 
