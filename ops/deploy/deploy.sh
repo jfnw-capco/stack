f162754efb479cea85c3bec3b2cea54fb953f4e4
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 10:38:20 +0000

    deploy and destroy changes

--- ops/deploy/deploy.sh
+++ ops/deploy/deploy.sh
@@ -7,10 +7,10 @@
     terraform init
 
 # Validates the plan    
-    terraform validate -var "token=${DO_TOKEN}" -var "image_id=${#1}" 
+    terraform validate -var "token=${DO_TOKEN}" -var "image_id=$1" 
 
 # Outputs the the plan
-    terraform plan -var "token=${DO_TOKEN}" -var "image_id=${#1}" -out=terraform.plan -lock=true
+    terraform plan -var "token=${DO_TOKEN}" -var "image_id=$1" -out=terraform.plan -lock=true
 
 # Performs the changes to the infrastructure
-    terraform apply -var "token=${DO_TOKEN}" -var "image_id=${#1}" -auto-approve -lock=true
+    terraform apply -var "token=${DO_TOKEN}" -var "image_id=$1" -auto-approve -lock=true
--- ops/deploy/destroy.sh
+++ ops/deploy/destroy.sh
@@ -2,6 +2,6 @@
 
 # Ensures runs in the current directory 
     cd $(dirname "$0")
-    
+
 # Performs the destroy 
-    terraform destroy -var "token=${DO_TOKEN}" -var "image_id=${#1}" -force -lock=true
+    terraform destroy -var "token=${DO_TOKEN}" -var "image_id=$1" -refresh=true -force -lock=true 
