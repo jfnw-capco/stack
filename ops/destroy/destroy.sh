7fb5dc55dac4fea822947e9304e53765c705216e
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 09:36:05 +0000

    Check in

--- ops/destroy/destroy.sh
+++ ops/destroy/destroy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Retrieves the Image ID
-    IMAGE_ID=$(cat ../provision/state/image_id.txt)
-
 # Performs the destroy 
-    terraform destroy -var "token=${DO_TOKEN}" -var "image_id=${IMAGE_ID}" -force -lock=true
+    terraform destroy -var "token=${DO_TOKEN}" -var "image_id=${#1}" -force -lock=true
