207d37ae70ec09be5cfe6af88fd79bae1db75c82
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 12:08:53 +0000

    Run

--- ops/provision/provision.sh
+++ ops/provision/provision.sh
@@ -4,10 +4,10 @@
     cd $(dirname "$0")
 
 # Gets the branch ID
-    BRANCH=$(git rev-parse —short HEAD)
+    BRANCH=$(git rev-parse --short HEAD)
 
 # Validate the Packer file
-    packer validate -var-file=node_config.json -var "do_token=${DO_TOKEN}" -var "branch=${BRANCH}" node.json
+    packer validate -var "do_token=${DO_TOKEN}" -var "branch=${BRANCH}" node.json
 
 # Runs the build
-    packer build -var -var-file=node_config.json -var "do_token=${DO_TOKEN}" -var "branch=${BRANCH}" node.json
+    packer build -var "do_token=${DO_TOKEN}" -var "branch=${BRANCH}" node.json
