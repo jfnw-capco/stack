8f1db861c4699045ca3e745cc71778483596e2f5
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-19 11:23:04 +0000

    Check in to ensure image_id is persisted

--- ops/provision/get_image_id.sh
+++ ops/provision/get_image_id.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ensures runs in the current directory 
+    cd $(dirname "$0")
+
 # Writes the current image
-    IMAGE_ID=$(jq -r .builds[].artifact_id ${#1})
-    echo ${IMAGE_ID#:}
+    IMAGE_ID=$(jq -r .builds[].artifact_id "node_manifest.json")
+    echo ${IMAGE_ID#:} >> "image_id.txt"
