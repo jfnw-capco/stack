103c67f2cb617df458e6d82c1b53cf88c0e74420
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-22 17:25:30 +0000

    Path changes

--- ops/provision/get_image_id.sh
+++ ops/provision/get_image_id.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Makes the directory if needed
-    mkdir -p ${1}
-
 # Ensures runs in the current directory 
     cd $(dirname "$0")
 
@@ -10,7 +7,4 @@
     IMAGE_ID=$(jq -r .builds[].artifact_id "${2}/${2}_manifest.json")
 
 # Changes to the state directory
-    cd ${1}
-    pwd
-    ls ${1}
-    echo ${IMAGE_ID#:} >> "${2}.id"
+    echo ${IMAGE_ID#:} >> "${1}/${2}.id"
