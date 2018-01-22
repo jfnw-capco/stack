86f22df98d86bdac40478c8913df494170c3be5d
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-22 14:17:43 +0000

    ensure map is passed
    Plus started to add the required draft docker swarm statements to the master file and understand the workflow better.

--- ops/provision/master/master_startup.sh
+++ ops/provision/master/master_startup.sh
@@ -14,3 +14,21 @@
 
 # Restart Docker to be sure
     systemctl restart docker
+
+# Create as the swarm master here 
+    # docker swarm init --advertise-addr <MANAGER-IP>
+
+# This will return the token to use for workers
+    # docker swarm join-token worker
+
+# Create a docker registry in the swarm
+    # docker service create --name registry --publish published=5000,target=5000 registry:2
+
+# Docker compose can be stood up 
+    # TODO here
+
+# In the directory where compose is
+    # docker-compose push
+
+# docker stack deploy --compose-file docker-compose.yml stackdemo
+    # Deploys across the swarm 
