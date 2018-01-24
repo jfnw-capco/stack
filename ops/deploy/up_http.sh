79fbd5975c872792c80dfd5285bf4e89c82ea364
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-24 17:48:41 +0000

    Reinstated the HTTP attempts

--- ops/deploy/up_http.sh
+++ ops/deploy/up_http.sh
@@ -1,9 +1,18 @@
 HTTP_STATUS=0
 
+TRIES=0
+
 while [ $HTTP_STATUS != 200 ]
 do
     HTTP_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" ${1})
     if [ $HTTP_STATUS != 200 ]; then
-         sleep 10s
+        TRIES=$((TRIES + 1))
+        echo "${TRIES} attempt connecting to ${1}..."
+        sleep 5s
+    fi
+
+    if [ "$TRIES" -gt "20" ]; then
+        echo "Number of tries exceeded"
+        exit 1
     fi
 done
