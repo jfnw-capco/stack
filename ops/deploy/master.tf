402469f36832f5e63348f8df16285b429114422d
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2018-01-24 13:17:37 +0000

    Enabled ipv6 on the master

--- ops/deploy/master.tf
+++ ops/deploy/master.tf
@@ -5,14 +5,15 @@ resource "digitalocean_tag" "master_tag" {
 
 # Create a new droplet
 resource "digitalocean_droplet" "master" {
-    image  = "${var.image_ids["master"]}"
-    name   = "${var.namespace}-${var.app}-${var.branch}-master-${count.index + 1}"
-    region = "${var.region}"
-    size   = "${var.size}"
-    tags   = ["${digitalocean_tag.master_tag.id}"]
-    private_networking = true
-    ssh_keys = "${var.keys}"
-    user_data = <<EOF
+    image               = "${var.image_ids["master"]}"
+    name                = "${var.namespace}-${var.app}-${var.branch}-master-${count.index + 1}"
+    region              = "${var.region}"
+    size                = "${var.size}"
+    tags                = ["${digitalocean_tag.master_tag.id}"]
+    private_networking  = true
+    ipv6                = true
+    ssh_keys            = "${var.keys}"
+    user_data           = <<EOF
 #!/bin/bash
 ./startup/startup.sh
 EOF
